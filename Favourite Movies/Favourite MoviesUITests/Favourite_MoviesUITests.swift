de090f442df593709d0d902970b2da8a03b341fd
Ashrith Jain <ashrithjain3524@gmail.com>
2022-07-10 19:32:06 +0530

    Commiting UI testCases

--- Favourite Movies/Favourite MoviesUITests/Favourite_MoviesUITests.swift
+++ Favourite Movies/Favourite MoviesUITests/Favourite_MoviesUITests.swift
@@ -8,35 +8,41 @@
 import XCTest
 
 class Favourite_MoviesUITests: XCTestCase {
-
+    var app: XCUIApplication!
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        try super.setUpWithError()
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
+    func testTableSwipe() throws {
+        //app.tables.cells.containing(.staticText, identifier:"Spider-Man: No Way Home").element.tap()
+        let tablesQuery = app.tables
+        let spoderCell = tablesQuery.cells.containing(.staticText, identifier:"Spider-Man: No Way Home").element
+        if spoderCell.exists{
+            XCTAssert(spoderCell.exists)
+            spoderCell.swipeUp()
+            XCTAssert(spoderCell.exists)
         }
+        
+    }
+    
+    func testTableTap() throws {
+        let tablesQuery = app.tables
+        let spoderCell = tablesQuery.cells.containing(.staticText, identifier:"Spider-Man: No Way Home").element
+        
+        if spoderCell.exists{
+            spoderCell.tap()
+            XCTAssert(app.navigationBars["Movie Detail"].exists)
+        }
+    }
+    
+    func testSearchBar() throws {
+        let searchField = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .searchField).element
+        let tablesQuery = app.tables
+        XCTAssert(searchField.exists)
+        XCTAssert(tablesQuery.cells.count > 0)
+        
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# FavMovies
+# Favourte Movies
+
+
+1. There are 2 screens in the Application Movie Listing, MovieDetail written in swift
+2. If no internet a pop up will apear in Listing Screen asking user to chose offline Mode or to Retry 
+3. Has offline Support using core data to save Data locally in offline mode does not support Images as of now
+4. Have used custom binding to bind variables
+5. TestCases writen for networkManager , request models, UI tests as of now
